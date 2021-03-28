fb743175a8352acda024de2e2bce6cb87ded7fef
DeLayne <delayne.labove@gmail.com>
2021-03-28 13:51:07 -0500

    created the form class to extend the wp widget class to setup widget

--- wordpress 4/wp-content/plugins/roi-calculator/roi-calculator.php
+++ wordpress 4/wp-content/plugins/roi-calculator/roi-calculator.php
@@ -24,7 +24,25 @@ define('ROI_CALC_LOCATION_URL', plugins_url('', __FILE__ ) );
 /*Function to register form widget with wordpress*/
 function roi_calc_register_form_widget(){
 	/*register the new widget called roi calc form widget*/
-	register_widget('ROI_CALC_FORM_WIDGET');
+	register_widget('ROI_CALC_FORM_WIDGET'); /*name of class*/
 }
 /*the function is hooked into the widget in its action*/
 add_action(wp_widgets_init(), 'roi_calc_register_form_widget');
+
+/*roi form widget class class that extends wordpress widget class*/
+class ROI_CALC_FORM_WIDGET extends WP_Widget{
+	/*1. CREATE SETUP FOR THE WIDGET*/
+	public function __construct( ) {
+		/*widget settings*/
+		$widget_ops=array(
+			'classname' => 'roi_calc_form',
+			'description' => __('Output the ROI Calculator Form' , 'roi_calc_register_form_widget'),
+		);
+		/*widget control settings*/
+		$control_ops = array(
+			'id_base' => 'roi_calc_form',
+		);
+
+		parent::__construct('roi_calc_form', 'ROI Calculator Form', $widget_ops, $control_ops);
+	}
+}
