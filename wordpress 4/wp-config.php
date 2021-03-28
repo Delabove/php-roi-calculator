d689e7d11a299bb6d8f0636f39433ffa2309027b
DeLayne <delayne.labove@gmail.com>
2021-03-28 10:41:32 -0500

    calculation form

---
+++ wordpress 4/wp-config.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'delayne_testing' );
+
+/** MySQL database username */
+define( 'DB_USER', 'root' );
+
+/** MySQL database password */
+define( 'DB_PASSWORD', '' );
+
+/** MySQL hostname */
+define( 'DB_HOST', 'localhost' );
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         '.5XZyylT6[F`OKu^Lk&Lc9i>*mhC|5dxmxobU?vJ]yde!( (l,UC2Ik|a7n`59`0' );
+define( 'SECURE_AUTH_KEY',  'FZH;fV[R2R`Fw.$bbB=4EcdxptR/q8v5[X)z(;#,y!$4>E}&WW]tihTUL$<1qv]^' );
+define( 'LOGGED_IN_KEY',    'go+,9T}[{XctC!EznptNwqXTD wE(~$=N|#1vgx&$i,V|.zPX5[~Z5,q>8h?u)Y&' );
+define( 'NONCE_KEY',        'FU;(xz~=iJE]B%XN(32prPJPdu&RW=vTu3FG|Hq5%yFJ?y{F8(zPP)LNVv1Q>X>,' );
+define( 'AUTH_SALT',        '3r;J2gso=v9,zYte)N4*cbGT?u^EI%N0d=LKow:<DO1SWD.lO*=~h3BTG]U~QVFJ' );
+define( 'SECURE_AUTH_SALT', 'BG}l5ZI;TfO:* `*1RvP_3jqD6+YYaV8Jo>}n5iz=BzNKQzPDFZizGmg?yu,l1f:' );
+define( 'LOGGED_IN_SALT',   '/3C.-nhv.n:u<L.O8`wp9GzlYGj8qfU.b;0SKS!ZUtHTATriL.u)]$QQ:SuP~oeB' );
+define( 'NONCE_SALT',       '[lQi)(c-Jn}++R|b{=]<Z2D7I5GNURlt{3y<CR)Z_[,PhTg1[l!iR4k~FVuD78w5' );
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
+ */
+define( 'WP_DEBUG', false );
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
---
+++ wordpress 4/wp-content/plugins/roi-calculator/dvs-roi-calculator.php
@@ -0,0 +1,66 @@
+<html>
+
+<head>
+	<title>Calculation Form</title>
+</head>
+
+<body>
+
+<form action = "calc.php" method = "post">
+
+	Value 1: <input type = "text" name = "val1" size = "10">
+	Value 2: <input type = "text" name = "val2" size = "10">
+	<br>
+	Calculation: <br>
+	<input type = "radio" name = "calc" value = "add"> Add
+	<input type = "radio" name = "calc" value = "sub"> Subtract
+	<input type = "radio" name = "calc" value = "mul"> Multiply
+	<input type = "radio" name = "calc" value = "div"> Divide
+	<hr>
+	<input type = "submit" value = "Calculate">
+	<input type = "reset" value = "Clear">
+
+</form>
+
+</body>
+
+</html>
+
+
+File: calc.php
+
+
+
+
+<html>
+<head>
+	<title>Calculation Result</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+<!---->
+<!---->
+<!--Related examples in the same category-->
+<!--1.	Deal with Array Form Data-->
+<!--2.	A process_form() Function-->
+<!--3.	Access widget's form value-->
+<!--4.	Accessing multiple submitted values-->
+<!--5.	Forms and PHP-->
+<!--6.	Feedback Form-->
+<!--7.	Handling Special Characters-->
+<!--8.	multivalue-forms.php-->
+<!--9.	Saying "Hello"-->
+<!--java2s.com  | © Demo Source and Support. All rights reserved.-->
